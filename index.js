e4db3c4b506a014cfe8923eaa4323ce6256cdf5a
Md Aminul Islam <mr.aminul118@gmail.com>
2024-12-22 08:02:55 +0600

    Food details by id

--- index.js
+++ index.js
@@ -5,14 +5,19 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Server is running.....");
 });
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b4uwa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,19 +31,50 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
 
     const usersCollections = client.db("FoddieDB").collection("users");
+    const foodCollections = client.db("FoddieDB").collection("foods");
 
+    //  Create a new user
     app.post("/users", async (req, res) => {
       const newUser = req.body;
+      const email = newUser.email;
+
+      // Find user by email
+      const find = await usersCollections.findOne({ email });
+      if (find) {
+        return res.status(409).send("User already exists"); // 409 Conflict
+      }
+      // console.log("New User Email:", email);
+      // Insert the new user into the database
       const result = await usersCollections.insertOne(newUser);
-      res.send(result);
+      res.status(201).send(result); // 201 Created
     });
+
+    // GET /users - Get all users
     app.get("/users", async (req, res) => {
-      const result = await usersCollections.findOne();
-      res.send(result);
+      const users = await usersCollections.findOne();
+      res.status(200).send(users); // 200 OK
+    });
+
+    // Foods collections
+    app.post("/add-food", async (req, res) => {
+      const newFood = req.body;
+      const result = await foodCollections.insertOne(newFood);
+      res.status(201).send(result);
+    });
+
+    app.get("/foods", async (req, res) => {
+      const result = await foodCollections.find().toArray();
+      res.status(200).send(result);
+    });
+    app.get("/food/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await foodCollections.findOne(query);
+      res.status(200).send(result);
     });
 
     // Send a ping to confirm a successful connection
@@ -46,11 +82,11 @@ async function run() {
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+  } catch (error) {
+    console.error("Error during operation:", error);
   }
 }
+
 run().catch(console.dir);
 
-app.listen(port, () => console.log("Server is ruuning on port", port));
+app.listen(port, () => console.log("Server is running on port", port));
